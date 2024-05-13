745dd3be23976378a34c0c002f6d273d24b2edb1
salman9faris <salman9faris@gmail.com>
2024-05-13 19:50:12 +0400

    added settings scree

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -6,11 +6,29 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.all(8),
-        child: Text(
-          "Settings",
-          style: kWelcometext,
-        ));
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(8),
+          child: Text(
+            "Settings",
+            style: kWelcometext,
+          ),
+        ),
+        Container(
+          height: 200,
+          width: 200,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  "assests/images/user.jpg",
+                ),
+                fit: BoxFit.contain),
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+        ),
+      ],
+    );
   }
 }
